38b0f4f26a4fad8207abfd5cd6d542afbafc304e
seema <seemarughade1363@gmail.com>
2024-08-18 23:01:33 +0530

    code updated

--- src/admin/ContactPage/ContactusAdmin.jsx
+++ src/admin/ContactPage/ContactusAdmin.jsx
@@ -7,7 +7,7 @@ const ContactUsAdmin = () => {
     useEffect(() => {
         const fetchContacts = async () => {
             try {
-                const response = await axios.get('https://arrc-backend.onrender.com/api/v1/contact');
+                const response = await axios.get('https://arrc-backend.onrender.com/api/v1/contacts');
                 setContacts(response.data);
             } catch (error) {
                 console.error('Error fetching contacts:', error);
