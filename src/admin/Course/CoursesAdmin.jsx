580080314583c46b6852071a25027e384c5f3e1a
seema <seemarughade1363@gmail.com>
2024-09-04 17:32:31 +0530

    last updated

--- src/admin/Course/CoursesAdmin.jsx
+++ src/admin/Course/CoursesAdmin.jsx
@@ -263,18 +263,19 @@ const CourseAdmin = () => {
                 <td className="py-2 px-4 border-b">{app.mode}</td>
                 <td className="py-2 px-4 border-b">{new Date(app.date).toLocaleDateString()}</td>
                 <td className="py-2 px-4 border-b flex space-x-2">
+                <button
+                    className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 flex items-center"
+                    onClick={() => handleEdit(app)}
+                  >
+                    <EditOutlined />
+                  </button>
                   <button
                     className="bg-red-500 text-white px-3 py-1 rounded hover:bg-red-600 flex items-center"
                     onClick={() => handleDelete(app._id)}
                   >
                     <DeleteOutlined />
                   </button>
-                  <button
-                    className="bg-blue-500 text-white px-3 py-1 rounded hover:bg-blue-600 flex items-center"
-                    onClick={() => handleEdit(app)}
-                  >
-                    <EditOutlined />
-                  </button>
+                  
                 </td>
               </tr>
             ))
